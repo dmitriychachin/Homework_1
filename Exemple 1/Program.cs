48d705b367be2c22c15e9183b9032389d7acef20
Dima Chachin <chachin99@bk.ru>
2022-11-27 22:10:53 +0300

    Exemple 1

---
+++ Exemple 1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число:");
+int num1 = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = Int32.Parse(Console.ReadLine());
+
+if(num1 > num2){
+    Console.WriteLine($"max = {num1}");
+}else if (num1 < num2){
+    Console.WriteLine($"max = {num2}");
+}else{
+    Console.WriteLine($"max = {num1}");
+};
---
+++ Exemple 2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число №1");
+int num1 = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число №2");
+int num2 = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число №3");
+int num3 = Int32.Parse(Console.ReadLine());
+
+int max = num1;
+
+if (max < num2){
+    max = num2;
+} else if (max < num3){
+    max = num3;
+} else {
+    max = max;
+}
+Console.WriteLine($"max = {max}");
