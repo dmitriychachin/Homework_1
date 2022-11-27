2e835186a570b4870f77900bd5a5f16a596a4814
Dima Chachin <chachin99@bk.ru>
2022-11-27 22:32:09 +0300

    Homework

---
+++ Exemple 3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите число");
+
+int num = Int32.Parse(Console.ReadLine());
+
+int result = num % 2;
+
+if (result == 0){
+    Console.WriteLine($"Число {num} четное");
+} else {
+    Console.WriteLine($"Число {num} нечетное");
+}
---
+++ Exemple 4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+
+int num = Int32.Parse(Console.ReadLine());
+
+for (int i = 2; i < num; i = i + 2)
+{
+    Console.Write($"{i},");
+}
+
+Console.Write($"{num}");
